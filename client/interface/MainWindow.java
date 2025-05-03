5ca8df328a23400cf8001b7c0244804a7493107c
Hiras Picano <hiraspicano@gmail.com>
2025-05-03 20:31:04 +0200

    Starting interface

---
+++ client/interface/MainWindow.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class MainWindow {
+    public MainWindow(){
+        JPanel TextFieldPanel = new JPanel();
+        JTextField MainField = new JTextField();
+        TextFieldPanel.add(MainField);
+    }
+}
